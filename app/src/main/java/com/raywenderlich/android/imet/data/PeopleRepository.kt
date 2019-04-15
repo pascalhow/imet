cb0f2c41b14632547d78d0d48764c6757013d99c
pascalhow <pascal.how@mydrivesolutions.com>
2019-04-15 15:43:48 +0100

    Create People Repository, Dao, Database

--- app/src/main/java/com/raywenderlich/android/imet/data/PeopleRepository.kt
+++ app/src/main/java/com/raywenderlich/android/imet/data/PeopleRepository.kt
@@ -34,49 +34,33 @@
 package com.raywenderlich.android.imet.data
 
 import android.app.Application
+import com.raywenderlich.android.imet.data.db.PeopleDao
+import com.raywenderlich.android.imet.data.db.PeopleDatabase
 import com.raywenderlich.android.imet.data.model.People
 import com.raywenderlich.android.imet.data.net.PeopleInfoProvider
 
 class PeopleRepository(application: Application) {
 
-  /**
-   * Returns the list of all people in reverse order (latest on top)
-   */
-  fun getAllPeople(): List<People> {
-    val allPeople = PeopleInfoProvider.peopleList
-    return allPeople.reversed()
-  }
+    private val peopleDao: PeopleDao
 
-  /**
-   * Adds a new people info on peopleList
-   */
-  fun insertPeople(people: People) {
-    PeopleInfoProvider.peopleList.add(people)
-  }
-
-  /**
-   * Finds people with specific id
-   */
-  fun findPeople(id: Int): People? {
-    for (people in PeopleInfoProvider.peopleList) {
-      if (people.id == id) {
-        return people
-      }
+    init {
+        val peopleDatabase = PeopleDatabase.getInstance(application)
+        peopleDao = peopleDatabase.peopleDao()
     }
-    return null
-  }
 
-  /**
-   * Finds people with similar name
-   */
-  fun searchPeople(name: String): List<People> {
-    var peoples = mutableListOf<People>()
-    for (people in PeopleInfoProvider.peopleList) {
-      if (people.name.toLowerCase().contains(name.toLowerCase())) {
-        peoples.add(people)
-      }
-    }
-    return peoples
-  }
+    /**
+     * Returns the list of all people in reverse order (latest on top)
+     */
+    fun getAllPeople(): List<People> = peopleDao.getAll()
+
+    /**
+     * Adds a new people info on peopleList
+     */
+    fun insertPeople(people: People) = peopleDao.insert(people)
+
+    /**
+     * Finds people with specific id
+     */
+    fun findPeople(id: Int): People? = peopleDao.find(id)
 
 }
---
+++ app/src/main/java/com/raywenderlich/android/imet/data/db/PeopleDao.kt
@@ -0,0 +1,28 @@
+package com.raywenderlich.android.imet.data.db
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import com.raywenderlich.android.imet.data.model.People
+
+@Dao
+interface PeopleDao {
+
+    //  1: Select All
+    @Query("SELECT * FROM People ORDER by id DESC")
+    fun getAll(): List<People>
+
+    //  2: Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(people: People)
+
+    //  3: Delete
+    @Query("DELETE FROM People")
+    fun deleteAll()
+
+    //4: Select by id
+    @Query("SELECT * FROM People WHERE id = :id")
+    fun find(id: Int): People
+
+}
